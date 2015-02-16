773b16c6d97d518d0db21d5d7eca4a0b50f96abe
Dennis Charles Hackethal <dennis.hackethal@gmail.com>
2015-02-16 14:40:53 -0800

    Implement Node

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,1 +1,15 @@
 require 'spec_helper'
+
+RSpec.describe Node do
+  let(:node) { Node.new('foo') }
+
+  describe 'Initialization' do
+    it 'assigns a value' do
+      expect(node.value).to eq 'foo'
+    end
+
+    it 'assigns nil to next_node' do
+      expect(node.next_node).to be_nil
+    end
+  end
+end
